8b5afbc624509ebe7f60477f94e4e587f63f5b75
Stenio Pereira <steniocp@gmail.com>
2018-04-28 09:52:16 -0700

    FF Deployment

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'mock' data
+var friendList = [
+  {
+    name: 'Stenio Pereira',
+    profilePic: 'https://avatars3.githubusercontent.com/u/17751317?v=3&s=466',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
